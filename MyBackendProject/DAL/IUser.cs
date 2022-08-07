94524836e975335928ad8242dd28bf21d037743e
JuliBima <julibima45@gmail.com>
2022-08-07 20:22:12 +0700

    Add project files.

---
+++ MyBackendProject/DAL/IUser.cs
@@ -0,0 +1,11 @@
+﻿using MyBackendProject.DTO;
+
+namespace MyBackendProject.DAL
+{
+    public interface IUser
+    {
+        Task Registration(CreateUserDto user);
+        Task<UserDto> Authenticate(string username, string password);
+        Task<IEnumerable<UserDto>> GetAll();
+    }
+}
---
+++ MyBackendProject/DTO/CreateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBackendProject.DTO
+{
+    public class CreateUserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
