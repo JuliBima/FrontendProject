3eb0e60df13ffd0f4f23894783ff7f66af53ff17
JuliBima <julibima45@gmail.com>
2022-08-08 15:53:49 +0700

    Menambahkan Layout

--- FrontendProject/Program.cs
+++ FrontendProject/Program.cs
@@ -3,6 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Session
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "mysession.fronted";
+    options.IdleTimeout = TimeSpan.FromMinutes(5);
+    options.Cookie.IsEssential = true;
+});
+
+
+//menambahkan agar pengguna yang belum login diminta login dan dapat mengakses controller
+builder.Services.ConfigureApplicationCookie(opt => opt.LoginPath = "/Account/Login");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,15 +25,20 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
+
+app.UseSession();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
